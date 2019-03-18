47ce0d7a0d34c1499030cc542571f2678f3da2a9
VNyre <valerie.nyre@t-online.de>
2019-03-18 19:42:49 +0100

    module fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,10 @@
 import { Injector, NgModule } from '@angular/core';
 import { createCustomElement } from '@angular/elements';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
 import { ButtonComponent } from './button/button.component';
 
 @NgModule({
     declarations: [
-        AppComponent,
         ButtonComponent,
     ],
     imports: [
@@ -16,12 +14,11 @@ import { ButtonComponent } from './button/button.component';
     providers: []
 })
 export class AppModule {
-    constructor(private injector: Injector) { }
+    constructor(private injector: Injector) {
+      const customElement = createCustomElement(ButtonComponent, {injector: this.injector});
+      customElements.define('oxs-element-button', customElement); }
 
-    ngDoBootstrap() {
-        const customElement = createCustomElement(ButtonComponent, {injector: this.injector});
-        customElements.define('oxs-element-button', customElement);
-    }
+    ngDoBootstrap() { }
 }
 
 
